fcccf27d1ae1bb33798bb01efa97b5885cdb0339
JCollins0 <jrc.collins01@gmail.com>
2021-12-30 13:50:26 -0600

    Simple tiled scene and camera movement using wasd for testing

---
+++ UnitySharedGame/Assets/Scripts/TestCameraController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right);
+        }
+    }
+}
