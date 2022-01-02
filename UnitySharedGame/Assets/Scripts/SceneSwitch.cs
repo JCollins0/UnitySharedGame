09cd184b769a38ce365abe501aa26d8851ed8166
Elizabeth Collins <ecnus.collins@gmail.com>
2022-01-02 12:39:03 -0600

    fixed duplicate of player (temporary)

--- UnitySharedGame/Assets/Scripts/SceneSwitch.cs
+++ UnitySharedGame/Assets/Scripts/SceneSwitch.cs
@@ -6,14 +6,30 @@ using UnityEngine.SceneManagement;
 public class SceneSwitch : MonoBehaviour
 {
     GameObject PlayObj;
+    Vector3 StartingPosition;
     public MoveToScene ChangeScene = new MoveToScene();
-    public enum MoveToScene { Kitchen, Greenhouse, ColdStorage };
+    public enum MoveToScene { Kitchen, GreenHouse, ColdStorage };
+
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayObj = GameObject.Find("Chef_Player");
-        DontDestroyOnLoad(PlayObj);
+            if (GameObject.Find("Chef_Player") == null)
+            {
+                StartingPosition.x = 13.3f;
+                StartingPosition.y = 4.3f;
+                StartingPosition.z = 0f;
+                Debug.Log("Game Player is null");
+                PlayObj = Instantiate(Resources.Load("Chef_Player") as GameObject);
+                PlayObj.name = "Chef_Player";
+                PlayObj.transform.position = StartingPosition;
+                DontDestroyOnLoad(PlayObj);
+            }
+            else
+            {
+                Debug.Log("Good To Go");
+            }
+   
     }
 
     // Update is called once per frame
@@ -24,10 +40,9 @@ public class SceneSwitch : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        
+        PlayObj = GameObject.Find("Chef_Player");
         Vector3 PlyrPos = PlayObj.gameObject.transform.position;
         Scene CurrentScene = SceneManager.GetActiveScene();
-
            if (ChangeScene.ToString() == "Kitchen") {
 
                if (CurrentScene.name == "GreenHouse")
@@ -43,17 +58,15 @@ public class SceneSwitch : MonoBehaviour
                SceneManager.LoadScene(0);  
                Debug.Log(PlyrPos.x + "," + PlyrPos.y);
            }
-           else if (ChangeScene.ToString() == "Greenhouse")
+           else if (ChangeScene.ToString() == "GreenHouse")
            {
                SceneManager.LoadScene(1);
                PlyrPos.x = 14f;
                PlyrPos.y = 4.25f;
-
            }
            else if (ChangeScene.ToString() == "ColdStorage")
            {
                SceneManager.LoadScene(2);
-
            }
         PlayObj.gameObject.transform.position = PlyrPos;
     }
